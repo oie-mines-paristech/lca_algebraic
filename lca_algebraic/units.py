12f243f8f6750390b1cba6fd9d749113651c155d
Raphael Jolivet <raphael.jolivet@mines-paristech.fr>
2024-04-23 18:36:05 +0200

    Add auto_scale attribute to the Units registry, disabled by default to force explicit conversion

--- lca_algebraic/units.py
+++ lca_algebraic/units.py
@@ -1,5 +1,9 @@
+import operator
+
 import brightway2 as bw
-from pint import DimensionalityError, UnitRegistry
+from pint import DimensionalityError, OffsetUnitCalculusError, UnitRegistry
+from pint.compat import _to_magnitude, zero_or_nan
+from pint.facets.plain import PlainQuantity
 
 from .base_utils import getActByCode
 
@@ -71,3 +75,125 @@ for unit in NEW_UNITS:
 
 for key, val in UNIT_ALIASES.items():
     unit_registry.define(f"@alias {val} = {key}")
+
+# Hack of Pint unit to fail when autoscale is false
+unit_registry.auto_scale = False
+
+
+def _add_sub_modified(self: PlainQuantity, other, op):
+    """Perform addition or subtraction operation and return the result.
+
+    Parameters
+    ----------
+    other : pint.PlainQuantity or any type accepted by :func:`_to_magnitude`
+        object to be added to / subtracted from self
+    op : function
+        operator function (e.g. operator.add, operator.isub)
+    """
+
+    def _safe_other_magnitude(units):
+        if not self._REGISTRY.auto_scale:
+            raise (Exception(f"Auto scale disabled : explicit convertio of '{other}' to {units} required"))
+        return other.to(units).magnitude
+
+    if not self._check(other):
+        # other not from same Registry or not a PlainQuantity
+        if zero_or_nan(other, True):
+            # If the other value is 0 or NaN (but not a PlainQuantity)
+            # do the operation without checking units.
+            # We do the calculation instead of just returning the same
+            # value to enforce any shape checking and type casting due to
+            # the operation.
+            units = self._units
+            magnitude = op(
+                self._magnitude,
+                _to_magnitude(other, self.force_ndarray, self.force_ndarray_like),
+            )
+        elif self.dimensionless:
+            units = self.UnitsContainer()
+            magnitude = op(
+                self.to(units)._magnitude,
+                _to_magnitude(other, self.force_ndarray, self.force_ndarray_like),
+            )
+        else:
+            raise DimensionalityError(self._units, "dimensionless")
+        return self.__class__(magnitude, units)
+
+    if not self.dimensionality == other.dimensionality:
+        raise DimensionalityError(self._units, other._units, self.dimensionality, other.dimensionality)
+
+    # Next we define some variables to make if-clauses more readable.
+    self_non_mul_units = self._get_non_multiplicative_units()
+    is_self_multiplicative = len(self_non_mul_units) == 0
+    if len(self_non_mul_units) == 1:
+        self_non_mul_unit = self_non_mul_units[0]
+    other_non_mul_units = other._get_non_multiplicative_units()
+    is_other_multiplicative = len(other_non_mul_units) == 0
+    if len(other_non_mul_units) == 1:
+        other_non_mul_unit = other_non_mul_units[0]
+
+    # Presence of non-multiplicative units gives rise to several cases.
+    if is_self_multiplicative and is_other_multiplicative:
+        if self._units == other._units:
+            magnitude = op(self._magnitude, other._magnitude)
+            units = self._units
+        # If only self has a delta unit, other determines unit of result.
+        elif self._get_delta_units() and not other._get_delta_units():
+            magnitude = op(self._convert_magnitude_not_inplace(other._units), other._magnitude)
+            units = other._units
+        else:
+            units = self._units
+            magnitude = op(self._magnitude, _safe_other_magnitude(self._units))
+
+    elif (
+        op == operator.sub
+        and len(self_non_mul_units) == 1
+        and self._units[self_non_mul_unit] == 1
+        and not other._has_compatible_delta(self_non_mul_unit)
+    ):
+        if self._units == other._units:
+            magnitude = op(self._magnitude, other._magnitude)
+        else:
+            magnitude = op(self._magnitude, _safe_other_magnitude(self._units))
+        units = self._units.rename(self_non_mul_unit, "delta_" + self_non_mul_unit)
+
+    elif (
+        op == operator.sub
+        and len(other_non_mul_units) == 1
+        and other._units[other_non_mul_unit] == 1
+        and not self._has_compatible_delta(other_non_mul_unit)
+    ):
+        # we convert to self directly since it is multiplicative
+        magnitude = op(self._magnitude, _safe_other_magnitude(self._units))
+        units = self._units
+
+    elif (
+        len(self_non_mul_units) == 1
+        # order of the dimension of offset unit == 1 ?
+        and self._units[self_non_mul_unit] == 1
+        and other._has_compatible_delta(self_non_mul_unit)
+    ):
+        # Replace offset unit in self by the corresponding delta unit.
+        # This is done to prevent a shift by offset in the to()-call.
+        tu = self._units.rename(self_non_mul_unit, "delta_" + self_non_mul_unit)
+        magnitude = op(self._magnitude, _safe_other_magnitude(tu))
+        units = self._units
+    elif (
+        len(other_non_mul_units) == 1
+        # order of the dimension of offset unit == 1 ?
+        and other._units[other_non_mul_unit] == 1
+        and self._has_compatible_delta(other_non_mul_unit)
+    ):
+        # Replace offset unit in other by the corresponding delta unit.
+        # This is done to prevent a shift by offset in the to()-call.
+        tu = other._units.rename(other_non_mul_unit, "delta_" + other_non_mul_unit)
+        magnitude = op(self._convert_magnitude_not_inplace(tu), other._magnitude)
+        units = other._units
+    else:
+        raise OffsetUnitCalculusError(self._units, other._units)
+
+    return self.__class__(magnitude, units)
+
+
+# Override the _add_sub method
+PlainQuantity._add_sub = _add_sub_modified
