fd319e49de26e275204c4a31aa9d5eedade5b58a
Benoît GSCHWIND <benoit.gschwind@mailbox.org>
2026-08-24 16:37:33 +0200

    Add test_simplify_min_max

--- test/test_stats.py
+++ test/test_stats.py
@@ -1,6 +1,8 @@
 import pytest
 import sympy
 
+from sympy import (Symbol, Min, Max)
+
 from lca_algebraic import newFloatParam
 from numpy import array
 
@@ -51,3 +53,31 @@ def test_simplify_products():
 
     assert e0sum == e0
     assert e0prd == (c - d)
+
+
+def test_simplify_min_max():
+    a = newFloatParam('a', min= 1.0, max= 2.0, default= 1.5)
+    b = newFloatParam('b', min=-2.0, max=-1.0, default=-1.5)
+    c = newFloatParam('c', min= 3.0, max= 4.0, default= 3.5)
+    d = newFloatParam('d', min=-4.0, max=-3.0, default=-3.5)
+
+    e0 = Min(a, b, c, d)
+    e1 = Max(a, b, c, d)
+
+    param_values = {
+        'a': array([a.min, a.max]),
+        'b': array([b.min, b.max]),
+        'c': array([c.min, c.max]),
+        'd': array([d.min, d.max]),
+    }
+
+    import lca_algebraic.stats
+    e0min = lca_algebraic.stats._simplify_min(e0, param_values)
+    e0max = lca_algebraic.stats._simplify_max(e0, param_values)
+    e1min = lca_algebraic.stats._simplify_min(e1, param_values)
+    e1max = lca_algebraic.stats._simplify_max(e1, param_values)
+
+    assert e0min == d
+    assert e0max == e0
+    assert e1min == e1
+    assert e1max == c
